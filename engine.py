a49c7c46dfb6cd46c1b9f9ad387d1ee5dabbc809
Samyak S Sarnayak <samyak201@gmail.com>
2020-04-16 17:19:45 +0530

    Implemented a basic SimPy simulation
     - Updated the Person and Community classes to add SimPy process
        functionality into it.
     - Also added some extra parameters needed to simulate the people
     - Updated `engine.py` with a small sample Community, it can be tested
        by directly running the file.

--- engine.py
+++ engine.py
@@ -5,8 +5,19 @@ import world
 
 def main():
     # Entire simulation process, must make all the data here available to the gui
-    environ = simpy.Environment()
+    env = simpy.Environment()
 
+    # simulating one (small) sample community for now
+    boundaries = ((0, 10), (0, 10))  # boundaries for the sample community
+
+    sample_community = world.Community(boundaries, env, no_of_people=10)
+    sample_community.activate()
+
+    # initially used env.step to run the environment, but that had a weird side effect
+    # of not actually updating the environment time (env.now)
+    for step in range(100):
+        print("Step {}".format(step))
+        env.run(until=env.now+1) # proceed by one time step
 
 if __name__ == "__main__":
     main()
